3147680676c42750109d352f42a77e25ba74b9d3
Vansh <vansh8755381346@gmail.com>
2022-08-06 08:40:45 +0530

    new

--- src/Comp/Foot.jsx
+++ src/Comp/Foot.jsx
@@ -3,7 +3,7 @@ import { MDBFooter, MDBContainer, MDBRow, MDBCol, MDBIcon } from 'mdb-react-ui-k
 
 export default function App() {
   return (
-    <MDBFooter className='foot' style={{backgroundColor:"green"}}>
+    <MDBFooter className='foot' style={{backgroundColor:"green",color:"white"}}>
       <section className='d-flex justify-content-center justify-content-lg-between p-4 border-bottom'>
         <div className='me-5 d-none d-lg-block'>
           <span>Get connected with us on social networks:</span>
